178fc675e4fa17228f42ff009ae4ed0881894214
KienanUnique <69349336+KienanUnique@users.noreply.github.com>
2022-11-12 15:21:39 +0500

    Add missing files

---
+++ Client/FormClient.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Client
+{
+    partial class FormClient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBoxChat = new System.Windows.Forms.RichTextBox();
+            this.textBoxMessageInput = new System.Windows.Forms.TextBox();
+            this.buttonSendMessage = new System.Windows.Forms.Button();
+            this.panelChat = new System.Windows.Forms.Panel();
+            this.panelConnection = new System.Windows.Forms.Panel();
+            this.labelNick = new System.Windows.Forms.Label();
+            this.textBoxNick = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelIP = new System.Windows.Forms.Label();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.textBoxIP = new System.Windows.Forms.TextBox();
+            this.buttonConnectionStatusChange = new System.Windows.Forms.Button();
+            this.panelChat.SuspendLayout();
+            this.panelConnection.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBoxChat
+            // 
+            this.richTextBoxChat.Location = new System.Drawing.Point(6, 3);
+            this.richTextBoxChat.Name = "richTextBoxChat";
+            this.richTextBoxChat.ReadOnly = true;
+            this.richTextBoxChat.Size = new System.Drawing.Size(751, 358);
+            this.richTextBoxChat.TabIndex = 0;
+            this.richTextBoxChat.Text = "";
+            this.richTextBoxChat.TextChanged += new System.EventHandler(this.richTextBoxChat_TextChanged);
+            // 
+            // textBoxMessageInput
+            // 
+            this.textBoxMessageInput.Location = new System.Drawing.Point(6, 367);
+            this.textBoxMessageInput.Name = "textBoxMessageInput";
+            this.textBoxMessageInput.Size = new System.Drawing.Size(670, 20);
+            this.textBoxMessageInput.TabIndex = 1;
+            this.textBoxMessageInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxMessageInput_KeyDown);
+            // 
+            // buttonSendMessage
+            // 
+            this.buttonSendMessage.Location = new System.Drawing.Point(682, 367);
+            this.buttonSendMessage.Name = "buttonSendMessage";
+            this.buttonSendMessage.Size = new System.Drawing.Size(75, 23);
+            this.buttonSendMessage.TabIndex = 2;
+            this.buttonSendMessage.Text = "Send";
+            this.buttonSendMessage.UseVisualStyleBackColor = true;
+            this.buttonSendMessage.MouseClick += new System.Windows.Forms.MouseEventHandler(this.buttonSendMessage_MouseClick);
+            // 
+            // panelChat
+            // 
+            this.panelChat.Controls.Add(this.textBoxMessageInput);
+            this.panelChat.Controls.Add(this.richTextBoxChat);
+            this.panelChat.Controls.Add(this.buttonSendMessage);
+            this.panelChat.Enabled = false;
+            this.panelChat.Location = new System.Drawing.Point(12, 48);
+            this.panelChat.Name = "panelChat";
+            this.panelChat.Size = new System.Drawing.Size(760, 390);
+            this.panelChat.TabIndex = 3;
+            // 
+            // panelConnection
+            // 
+            this.panelConnection.Controls.Add(this.labelNick);
+            this.panelConnection.Controls.Add(this.textBoxNick);
+            this.panelConnection.Controls.Add(this.label2);
+            this.panelConnection.Controls.Add(this.labelIP);
+            this.panelConnection.Controls.Add(this.textBoxPort);
+            this.panelConnection.Controls.Add(this.textBoxIP);
+            this.panelConnection.Location = new System.Drawing.Point(12, 3);
+            this.panelConnection.Name = "panelConnection";
+            this.panelConnection.Size = new System.Drawing.Size(615, 39);
+            this.panelConnection.TabIndex = 4;
+            // 
+            // labelNick
+            // 
+            this.labelNick.Location = new System.Drawing.Point(435, 6);
+            this.labelNick.Name = "labelNick";
+            this.labelNick.Size = new System.Drawing.Size(41, 23);
+            this.labelNick.TabIndex = 4;
+            this.labelNick.Text = "Nick";
+            this.labelNick.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // textBoxNick
+            // 
+            this.textBoxNick.Location = new System.Drawing.Point(482, 6);
+            this.textBoxNick.Name = "textBoxNick";
+            this.textBoxNick.Size = new System.Drawing.Size(125, 20);
+            this.textBoxNick.TabIndex = 3;
+            this.textBoxNick.Text = "Client";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(247, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Port:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // labelIP
+            // 
+            this.labelIP.Location = new System.Drawing.Point(3, 6);
+            this.labelIP.Name = "labelIP";
+            this.labelIP.Size = new System.Drawing.Size(29, 23);
+            this.labelIP.TabIndex = 1;
+            this.labelIP.Text = "IP:";
+            this.labelIP.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(294, 6);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(125, 20);
+            this.textBoxPort.TabIndex = 0;
+            this.textBoxPort.Text = "9000";
+            // 
+            // textBoxIP
+            // 
+            this.textBoxIP.Location = new System.Drawing.Point(38, 6);
+            this.textBoxIP.Name = "textBoxIP";
+            this.textBoxIP.Size = new System.Drawing.Size(188, 20);
+            this.textBoxIP.TabIndex = 0;
+            this.textBoxIP.Text = "127.0.0.1";
+            // 
+            // buttonConnectionStatusChange
+            // 
+            this.buttonConnectionStatusChange.Location = new System.Drawing.Point(633, 6);
+            this.buttonConnectionStatusChange.Name = "buttonConnectionStatusChange";
+            this.buttonConnectionStatusChange.Size = new System.Drawing.Size(136, 23);
+            this.buttonConnectionStatusChange.TabIndex = 4;
+            this.buttonConnectionStatusChange.Text = "Connect";
+            this.buttonConnectionStatusChange.UseVisualStyleBackColor = true;
+            this.buttonConnectionStatusChange.MouseClick += new System.Windows.Forms.MouseEventHandler(this.buttonConnectionStatusChange_MouseClick);
+            // 
+            // FormClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(807, 450);
+            this.Controls.Add(this.buttonConnectionStatusChange);
+            this.Controls.Add(this.panelConnection);
+            this.Controls.Add(this.panelChat);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FormClient";
+            this.Text = "Client";
+            this.panelChat.ResumeLayout(false);
+            this.panelChat.PerformLayout();
+            this.panelConnection.ResumeLayout(false);
+            this.panelConnection.PerformLayout();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.TextBox textBoxNick;
+        private System.Windows.Forms.Label labelNick;
+
+        private System.Windows.Forms.RichTextBox richTextBoxChat;
+        private System.Windows.Forms.Panel panelChat;
+
+        private System.Windows.Forms.TextBox textBoxIP;
+        private System.Windows.Forms.TextBox textBoxPort;
+        private System.Windows.Forms.Label labelIP;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonDisconnect;
+        private System.Windows.Forms.Button buttonConnectionStatusChange;
+
+        private System.Windows.Forms.Panel panelConnection;
+
+        private System.Windows.Forms.TextBox textBoxMessageInput;
+        private System.Windows.Forms.Button buttonSendMessage;
+
+        private System.Windows.Forms.RichTextBox richTextBoxLogs;
+
+        #endregion
+    }
+}
