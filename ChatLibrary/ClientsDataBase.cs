19b727099aa3e7c7543ec5ecf722387860808287
KienanUnique <69349336+KienanUnique@users.noreply.github.com>
2022-12-21 13:34:00 +0500

    Add first version of clients data base

---
+++ ChatLibrary/ClientsDataBase.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace ChatLibrary;
+
+public class ClientAlreadyExistException : Exception
+{
+    public ClientAlreadyExistException(string login) : base($"Client with this login already exists: {login}")
+    {
+    }
+}
+
+public class ClientDoesntExistException : Exception
+{
+    public ClientDoesntExistException(string login) : base($"Client with this login doesn't exists: {login}")
+    {
+    }
+}
+
+public class ClientsDataBase
+{
+    protected Dictionary<string, string> _loginsAndPasswords = new();
+
+    public void SignInNewUser(string login, string password)
+    {
+        if (_loginsAndPasswords.ContainsKey(login))
+        {
+            throw new ClientAlreadyExistException(login);
+        }
+
+        _loginsAndPasswords.Add(login, password);
+    }
+
+    public bool IsClientsPasswordCorrect(string login, string password)
+    {
+        if (!_loginsAndPasswords.ContainsKey(login))
+        {
+            throw new ClientDoesntExistException(login);
+        }
+
+        return _loginsAndPasswords[login] == password;
+    }
+}
+
+public class ClientsDataBaseWithFileStorage : ClientsDataBase
+{
+    private const string FileName = "ClientsData.txt";
+
+    public bool IsFileExists()
+    {
+        return File.Exists(FileName);
+    }
+
+    public void ReadClientsData()
+    {
+        var readText = File.ReadAllText(FileName);
+        _loginsAndPasswords = JsonSerializer.Deserialize<Dictionary<string, string>>(readText);
+    }
+
+    public void WriteClientsData()
+    {
+        var threeNumbersString = JsonSerializer.Serialize(_loginsAndPasswords) + Environment.NewLine;
+        File.WriteAllText(FileName, threeNumbersString);
+    }
+}
